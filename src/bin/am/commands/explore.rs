a1f5c7fdf75755419e2e643fad2bf10c2c494967
Mari <git@cutegirl.tech>
2023-07-14 15:30:14 +0200

    add `explore` subcommand
    fixes #74

---
+++ src/bin/am/commands/explore.rs
@@ -0,0 +1,69 @@
+use anyhow::Result;
+use autometrics_am::config::{endpoints_from_first_input, AmConfig, Endpoint};
+use autometrics_am::parser::endpoint_parser;
+use clap::Parser;
+use tracing::info;
+use url::Url;
+
+#[derive(Parser, Clone)]
+pub struct CliArguments {
+    /// The endpoint(s) that Prometheus will scrape.
+    ///
+    /// Multiple endpoints can be specified by separating them with a space.
+    /// The endpoint can be provided in the following formats:
+    /// - `:3000`. Defaults to `http`, `localhost` and `/metrics`.
+    /// - `localhost:3000`. Defaults to `http`, and `/metrics`.
+    /// - `https://localhost:3000`. Defaults to `/metrics`.
+    /// - `https://localhost:3000/api/metrics`. No defaults.
+    #[clap(value_parser = endpoint_parser, verbatim_doc_comment)]
+    metrics_endpoints: Vec<Url>,
+
+    /// Which endpoint to open in the browser
+    #[clap(long, env)]
+    explorer_endpoint: Option<Url>,
+}
+
+#[derive(Debug, Clone)]
+struct Arguments {
+    metrics_endpoints: Vec<Endpoint>,
+    explorer_endpoint: Url,
+}
+
+impl Arguments {
+    fn new(args: CliArguments, config: AmConfig) -> Self {
+        Arguments {
+            metrics_endpoints: endpoints_from_first_input(args.metrics_endpoints, config.endpoints),
+            explorer_endpoint: args
+                .explorer_endpoint
+                .or(config.explorer_endpoint)
+                .unwrap_or_else(|| Url::parse("http://localhost:6789/explorer").unwrap()), // .unwrap is safe because we control the input
+        }
+    }
+}
+
+pub async fn handle_command(args: CliArguments, config: AmConfig) -> Result<()> {
+    let mut args = Arguments::new(args, config);
+
+    let query: String = args
+        .metrics_endpoints
+        .into_iter()
+        .map(|e| format!("_prometheusUrl={}", e.url))
+        .collect::<Vec<_>>()
+        .join("&");
+
+    let url = &mut args.explorer_endpoint;
+    url.set_query(if !query.is_empty() {
+        Some(query.as_str())
+    } else {
+        None
+    });
+
+    if open::that(url.as_str()).is_err() {
+        info!(
+            "Unable to open browser, open the following URL in your browser: {}",
+            url.as_str()
+        );
+    }
+
+    Ok(())
+}
--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,7 @@
 use crate::parser::endpoint_parser;
 use serde::de::Error;
 use serde::{Deserialize, Deserializer};
+use std::sync::atomic::{AtomicUsize, Ordering};
 use url::Url;
 
 /// This struct represents the am.toml configuration. Most properties in here
@@ -12,6 +13,9 @@ pub struct AmConfig {
     /// The endpoints that will be scraped by the Prometheus server.
     #[serde(rename = "endpoint")]
     pub endpoints: Option<Vec<Endpoint>>,
+
+    /// Endpoint where explorer lives at
+    pub explorer_endpoint: Option<Url>,
 
     /// Startup the pushgateway.
     pub pushgateway_enabled: Option<bool>,
@@ -29,4 +33,41 @@ pub struct Endpoint {
 fn parse_maybe_shorthand<'de, D: Deserializer<'de>>(input: D) -> Result<Url, D::Error> {
     let input_str: String = Deserialize::deserialize(input)?;
     endpoint_parser(&input_str).map_err(Error::custom)
+}
+
+/// If the user specified an endpoint using args, then use those.
+/// Otherwise, use the endpoint configured in the config file. And
+/// fallback to an empty list if neither are configured.
+pub fn endpoints_from_first_input(args: Vec<Url>, config: Option<Vec<Endpoint>>) -> Vec<Endpoint> {
+    static COUNTER: AtomicUsize = AtomicUsize::new(0);
+
+    if !args.is_empty() {
+        args.into_iter()
+            .map(|url| {
+                let num = COUNTER.fetch_add(1, Ordering::SeqCst);
+                Endpoint {
+                    url,
+                    job_name: Some(format!("am_{num}")),
+                    honor_labels: Some(false),
+                }
+            })
+            .collect()
+    } else if let Some(endpoints) = config {
+        endpoints
+            .into_iter()
+            .map(|endpoint| {
+                let job_name = endpoint.job_name.unwrap_or_else(|| {
+                    format!("am_{num}", num = COUNTER.fetch_add(1, Ordering::SeqCst))
+                });
+
+                Endpoint {
+                    url: endpoint.url,
+                    job_name: Some(job_name),
+                    honor_labels: endpoint.honor_labels,
+                }
+            })
+            .collect()
+    } else {
+        Vec::new()
+    }
 }
